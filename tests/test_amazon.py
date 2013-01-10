3b7e3e3f598ef6d53c5f2aef682c35a968350ba4
Oskari Hiltunen <felix.loether@gmail.com>
2013-01-10 11:33:50 +0200

    Fix Amazon tests.

--- tests/test_amazon.py
+++ tests/test_amazon.py
@@ -1,21 +1,35 @@
 from __future__ import with_statement
+from datetime import datetime
 from pytest import raises
 
 from flexmock import flexmock
 from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+from boto.s3.bucket import Bucket
 from tests import TestCase
 from flask_storage import S3BotoStorage, S3BotoStorageFile, StorageException
 
 
 class MockKey(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
     def set_metadata(self, key, data):
         pass
 
     def set_contents_from_string(self, s, **kwargs):
         pass
 
+    def open(self, *args, **kwargs):
+        pass
+
+    last_modified = datetime(1971, 1, 1)
+
 
 class MockBucket(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
     def set_acl(self, acl):
         pass
 
@@ -32,22 +46,22 @@ class MockBucket(object):
         return []
 
 
-def mock_s3_connection():
-    return (
-        flexmock(S3Connection)
+def mock_s3():
+    flexmock(Key).should_receive('__new__').replace_with(MockKey)
+    flexmock(Bucket).should_receive('__new__').replace_with(MockBucket)
+    (flexmock(S3Connection)
         .should_receive('__init__')
-        .and_return(None)
-    )
+        .and_return(None))
 
 
 class TestS3BotoStorage(TestCase):
     def setup_method(self, method):
         TestCase.setup_method(self, method)
-        mock_s3_connection()
+        mock_s3()
         self.storage = S3BotoStorage()
 
     def test_assigns_folder_on_initialization(self):
-        mock_s3_connection()
+        mock_s3()
         storage = S3BotoStorage('some bucket')
         assert storage.folder_name == 'some bucket'
 
@@ -59,7 +73,7 @@ class TestS3BotoStorage(TestCase):
         self.storage.create_folder('some_folder')
 
     def test_delete_raises_exception_for_unknown_file(self):
-        mock_s3_connection()
+        mock_s3()
         (
             flexmock(S3Connection)
             .should_receive('get_bucket')
@@ -70,7 +84,7 @@ class TestS3BotoStorage(TestCase):
             storage.delete('key')
 
     def test_list_files_returns_list_of_key_names(self):
-        mock_s3_connection()
+        mock_s3()
         (
             flexmock(S3Connection)
             .should_receive('get_bucket')
@@ -80,7 +94,7 @@ class TestS3BotoStorage(TestCase):
         assert storage.list_files() == []
 
     def test_save_file_with_content_as_string(self):
-        mock_s3_connection()
+        mock_s3()
         (
             flexmock(S3Connection)
             .should_receive('get_bucket')
@@ -92,7 +106,7 @@ class TestS3BotoStorage(TestCase):
 
 class TestS3BotoStorageOpenFile(TestCase):
     def test_open_returns_file_object(self):
-        mock_s3_connection()
+        mock_s3()
 
         flexmock(S3BotoStorage) \
             .should_receive('_get_or_create_bucket') \
@@ -106,7 +120,7 @@ class TestS3BotoStorageOpenFile(TestCase):
 class TestS3BotoStorageFile(TestCase):
     def setup_method(self, method):
         TestCase.setup_method(self, method)
-        mock_s3_connection()
+        mock_s3()
         flexmock(S3BotoStorage) \
             .should_receive('_get_or_create_bucket') \
             .with_args('some_bucket') \
@@ -120,11 +134,13 @@ class TestS3BotoStorageFile(TestCase):
         assert bool(file_) is False
 
     def test_supports_prefixes(self):
+        mock_s3()
         file_ = S3BotoStorageFile(self.storage, prefix='pics/')
         file_.name = 'some_key'
         assert file_.name == u'pics/some_key'
 
     def test_supports_last_modified(self):
+        mock_s3()
         file_ = S3BotoStorageFile(self.storage, prefix='pics/')
         file_.name = 'some_key'
         file_.last_modified
